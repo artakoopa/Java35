9c7b6795bb9a28ba62ea01b4514edd1324440464
Arta Koopa <spridearta29@gmail.com>
2023-02-24 22:09:24 +0200

    HW5

--- src/homeworks/HM5.java
+++ src/homeworks/HM5.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HM5 {
     public static void main(String[] args) {
         // use not so popular math operation
@@ -18,5 +20,29 @@ public class HM5 {
         amount of jam specified by Jane. Amount of jam should be read from user
         (positive int value). You can assume that there's always enough jam in Alex's shop.
         Your application should print true or false.*/
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the amount of jam in kg");
+        int amount = scanner.nextInt();
+
+        int jar1  = 7;
+        int jar2 = 1;
+
+        int wholejar1 = amount / jar1;
+        //System.out.println(wholejar1);
+
+        int residualofjar1 = amount % jar1;
+       //System.out.println(residualofjar1);
+
+        int wholejar2 = residualofjar1 / jar2;
+        //System.out.println(wholejar2);
+
+
+        if ((wholejar1 + wholejar2) <= 6){
+            System.out.println(true);
+       } else {
+            System.out.println(false);
+       }
+
     }
 }
