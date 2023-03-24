bf3654b5fb6f9a0c54093e20dc1e0033faa7934a
Arta Koopa <spridearta29@gmail.com>
2023-03-24 20:44:14 +0200

    Abstraction, Inner and Outer classes, Nested classes

---
+++ src/oop/Abstraction/AbstractClassExample.java
@@ -0,0 +1,30 @@
+package oop.Abstraction;
+
+//Abstract class
+abstract class Animal{
+    protected String name;
+    //Regular method
+    public void sleep(){
+        System.out.println("ZZZZ");
+    }
+    //Abstract method
+    public abstract void animalSound();
+}
+
+class Pig extends Animal{
+    public void animalSound(){
+        System.out.println("oink oink");
+    }
+}
+
+public class AbstractClassExample {
+    public static void main(String[] args) {
+
+        //Cannot create an object from abstract class
+        //Animal animal1 = new Animal();
+
+        Pig pig = new Pig();
+        pig.sleep();
+        pig.animalSound();
+    }
+}
---
+++ src/oop/Abstraction/CallingFish.java
@@ -0,0 +1,9 @@
+package oop.Abstraction;
+
+public class CallingFish {
+    public static void main(String[] args) {
+
+        Fish fish = new Fish("Black", "Bread", "Water",4,2);
+        fish.printPetInfo();
+    }
+}
