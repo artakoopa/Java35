c9893326af549dc6faecb636e083ea61cb4c92d3
Arta Koopa <spridearta29@gmail.com>
2023-04-04 14:02:34 +0300

    HM17 part two

---
+++ src/homeworks/HM17/Car.java
@@ -0,0 +1,21 @@
+package homeworks.HM17;
+
+public class Car extends Vehicle {
+
+    private boolean airConditioner;
+
+    public Car(float fuel, float fuelUsage, int passengers, boolean airConditioner){
+        super(fuel, fuelUsage, passengers);
+        this.airConditioner = airConditioner;
+    }
+
+    public float maxDistance (float fuel, float fuelUsage, int passengers){
+        float fuelUsageWithPassenger = fuelUsage + (fuelUsage * (passengers * 0.05f));
+        if (airConditioner){
+            fuelUsageWithPassenger *= 1.1f;
+        }
+        return (fuel / fuelUsageWithPassenger) * 100;
+    }
+
+
+}
